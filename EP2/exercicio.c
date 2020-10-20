5f60c5ec353d43e311e006e2265abc4a0f0ac1c3
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-10-20 13:42:45 -0300

    refactor: adiciona pthread_join, mas não sei pra que serve

--- EP2/exercicio.c
+++ EP2/exercicio.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <error.h>
+#include <stdbool.h>
 
 void critical_section(int id);
 void non_critical_section(int id);
 
 int count = 0;
 int turn = 0;
-
-int true = 1;
-
 
 /* Função que será executada pelas threads */
 void *runner_1(void * param)
@@ -59,6 +57,9 @@ int main(int argc, char *argv[])
     
     pthread_create(&thread_1, NULL, runner_1, NULL);
     pthread_create(&thread_2, NULL, runner_2, NULL);
+    
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
 
     printf("\n[Fim do programa]\n\n");
 
@@ -68,12 +69,11 @@ int main(int argc, char *argv[])
 
 void critical_section(int id){
     printf("Thread %d entrou na seção crítica\n", id);
-    printf("Thread %d mudou count para %d\n", id, ++count);
+    printf("%d\n", ++count);
     printf("Thread %d saiu na seção crítica\n", id);
 }
 
 void non_critical_section(int id){
     printf("Thread %d entrou na seção não crítica\n", id);
-    printf("Thread %d está executando a seção não crítica...\n", id);
     printf("Thread %d saiu na seção não crítica\n", id);
 }
