cf2d30302b0f934201701c2bc5bb10859c0283ca
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-10-20 13:10:02 -0300

    fix: implementa usando posix thread mutex

--- EP2/exercicio.c
+++ EP2/exercicio.c
@@ -2,27 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <error.h>
+#include <stdbool.h>
 
 void critical_section(int id);
 void non_critical_section(int id);
 
 int count = 0;
-int turn = 0;
-
-int true = 1;
-
+pthread_mutex_t lock; 
 
 /* Função que será executada pelas threads */
 void *runner_1(void * param)
 {
     int id = 0;
-    int other = 1;
-    
-    while(true){
-        while(turn != id){}
 
+    while(true){
+        pthread_mutex_lock(&lock); 
         critical_section(id);
-        turn = other;
+        pthread_mutex_unlock(&lock); 
+
         non_critical_section(id);
     }
 
@@ -32,13 +29,12 @@ void *runner_1(void * param)
 void *runner_2(void * param)
 {
     int id = 1;
-    int other = 0;
-    
-    while(true){
-        while(turn != id){}
 
+    while(true){
+        pthread_mutex_lock(&lock); 
         critical_section(id);
-        turn = other;
+        pthread_mutex_unlock(&lock); 
+
         non_critical_section(id);
     }
 
@@ -48,6 +44,11 @@ void *runner_2(void * param)
 /* Thread principal do processo */
 int main(int argc, char *argv[])
 {
+    if (pthread_mutex_init(&lock, NULL) != 0) { 
+        printf("\n mutex init has failed\n"); 
+        return 1; 
+    }
+
     printf("[Inicio do programa]\n\n");
     pthread_t thread_1, thread_2;
     pthread_attr_t thread_attributes;
@@ -57,8 +58,13 @@ int main(int argc, char *argv[])
 
     /* Inicializa x threads através da função "pthread_create" */
     
-    pthread_create(&thread_1, NULL, runner_1, NULL);
-    pthread_create(&thread_2, NULL, runner_2, NULL);
+    pthread_create(&thread_1, NULL, &runner_1, NULL);
+    pthread_create(&thread_2, NULL, &runner_2, NULL);
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+
+    pthread_mutex_destroy(&lock); 
 
     printf("\n[Fim do programa]\n\n");
 
@@ -67,13 +73,13 @@ int main(int argc, char *argv[])
 }
 
 void critical_section(int id){
-    printf("Thread %d entrou na seção crítica\n", id);
+    printf("Thread %d entrou na sessão crítica\n", id);
     printf("Thread %d mudou count para %d\n", id, ++count);
-    printf("Thread %d saiu na seção crítica\n", id);
+    printf("Thread %d saiu na sessão crítica\n", id);
 }
 
 void non_critical_section(int id){
-    printf("Thread %d entrou na seção não crítica\n", id);
-    printf("Thread %d está executando a seção não crítica...\n", id);
-    printf("Thread %d saiu na seção não crítica\n", id);
+    printf("Thread %d entrou na sessão não crítica\n", id);
+    printf("Thread %d está executando a sessão não crítica...\n", id);
+    printf("Thread %d saiu na sessão não crítica\n", id);
 }
